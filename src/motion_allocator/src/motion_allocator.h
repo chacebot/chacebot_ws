ee8716708af9ad212e435f36cf7c7e7e44a3804b
chacebot <chace.medeiros@gmail.com>
2020-04-05 12:27:28 -0700

    started motion allocator node

---
+++ src/motion_allocator/src/motion_allocator.h
@@ -0,0 +1,29 @@
+#ifndef MOTION_ALLOCATOR_H 
+#define MOTION_ALLOCATOR_H
+
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+
+#include <memory>
+#include <thread>
+
+
+class MotionAllocator {
+
+public:
+
+MotionAllocator();
+~MotionAllocator();
+
+private:
+
+};
+
+
+
+
+
+
+#endif

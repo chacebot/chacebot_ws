c8ba86d78e06721c277d0a335b54021246ee5d66
chacebot <chace.medeiros@gmail.com>
2020-05-13 10:21:19 -0700

    Regex is up

--- src/local_controller/src/LocalController.cpp
+++ src/local_controller/src/LocalController.cpp
@@ -11,47 +11,27 @@ LocalController::LocalController(std::string port, ros::NodeHandle nh,
 
 void LocalController::readSerial()
 {
-  std::string result;
+  std::string raw_input;
   boost::asio::streambuf data;
-  int steering, drive, status;
-
   boost::asio::read_until(serial, data, '\n');
   std::istream is(&data);
-  std::getline(is, result);
-  std::cout<<"Pattern to match: " << result <<"\n";
+  std::getline(is, raw_input);
 
-  std::string raw_input = "<1500,1500,1500>";
+  static std::regex const inputPattern{"<(\\d{1,4}),(\\d{1,4}),(\\d{1,4})>\r"};
+  std::smatch matches;
 
-  std::cout <<"RawInput: " << raw_input << "\n";
-
-  //raw_input = result;
-
-static std::regex const inputPattern{"^<(\\d{1,4}),(\\d{1,4}),(\\d{1,4})>$"};
-
-   std::smatch matches;
-  
-   if (std::regex_search(raw_input, matches, inputPattern))
-  { 
-    std::cout<< "Matches: " << matches[1] << ", " << matches[2] << ", " << matches[3] <<"\n";
-    //auto const rawStatus = std::stoi(matches[3]);
-    //message.STEERING = rcConverter(std::stoi(matches[1]));
-    //message.DRIVE = rcConverter(std::stoi(matches[2]));
-    //message.STATUS = (status > 1900 && status < 2000) ? SafetyStatus::is_enabled : SafetyStatus::is_disabled;
+  if (std::regex_search(raw_input, matches, inputPattern))
+  {
+    auto const rawStatus = std::stoi(matches[3]);
+    message.STEERING = rcConverter(std::stoi(matches[1]));
+    message.DRIVE = rcConverter(std::stoi(matches[2]));
+    message.STATUS = (rawStatus > 1100 && rawStatus < 1200) ? SafetyStatus::is_enabled : SafetyStatus::is_disabled;
   }
   else
   {
-    std::cout <<"Regex does not read anything\n";
+    std::cout << "Regex does not read anything\n";
+    message.STATUS = SafetyStatus::is_disabled;
   }
-
-  
-  std::replace(result.begin(), result.end(), '<', ' ');
-  std::replace(result.begin(), result.end(), ',', ' ');
-  std::replace(result.begin(), result.end(), '>', ' ');
-  std::istringstream inputstream(result);
-  inputstream >> steering >> drive >> status;
-  message.STEERING = rcConverter(steering);
-  message.DRIVE = rcConverter(drive);
-  message.STATUS = (status > 1900 && status < 2000) ? SafetyStatus::is_enabled : SafetyStatus::is_disabled;
 }
 
 const float LocalController::rcConverter(const int &ppm)
